02832df14ea39ef4e27d0f59fd1e7ad2ce98aca0
mehadi07 <saimum22@gmail.com>
2017-04-03 01:46:42 -0400

    More

--- Event Reg/app/js/controllers/EventController.js
+++ Event Reg/app/js/controllers/EventController.js
@@ -2,17 +2,39 @@
 
 
 eventsApp.controller('EventController',
-                    
-    function EventController($scope){
-   $scope.event = {
-       name: 'Angular Event App',
-       date: '04/02/2017',
-       time: '3.09 am',
-       location: {
-           address: 'Google Headquaters',
-           city: 'Mountain View',
-           Province: 'CA'
-       },
-       imageUrl: '/img/angularjs-logo.png'
-   } 
-});
+
+    function EventController($scope) {
+        $scope.event = {
+            name: 'Angular Event App',
+            date: '04/02/2017',
+            time: '3.09 am',
+            location: {
+                address: 'Google Headquaters',
+                city: 'Mountain View',
+                Province: 'CA'
+            },
+            imageUrl: '/img/angularjs-logo.png',
+            sessions: [
+                {
+                    name: 'Directive Masterclass',
+                    creatorName: 'Bob Smith',
+                    Duration: '1 hr',
+                    level: 'advance'
+           },
+                {
+                    name: 'Scope for fun and profit',
+                    creatorName: 'Jhon Doe',
+                    Duration: '45 mins',
+                    level: 'advance'
+
+           },
+                {
+                    name: 'well Behaved Controllers',
+                    creatorName: 'Md Mehadi Hasan Mozumder',
+                    Duration: '1 hr',
+                    level: 'advance'
+
+           }
+       ]
+        };
+    });
