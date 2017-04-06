074757f3c7b2e7aa669ef1620ba55db965a578a5
mehadi07 <saimum22@gmail.com>
2017-04-06 19:51:26 -0400

    Validation

--- Event Reg/app/js/controllers/EditEventController.js
+++ Event Reg/app/js/controllers/EditEventController.js
@@ -3,8 +3,11 @@
 eventsApp.controller('EditEventController',
     function EditEventController($scope) {
 
-        $scope.saveEvent = function (event) {
-            window.alert('event' + event.name + 'saved !');
+        $scope.saveEvent = function (event, newEventForm) {
+            if (newEventForm.$valid) {
+                window.alert('event' + event.name + 'saved !');
+
+            }
         };
         $scope.cancelEdit = function () {
             window.location = "/EventDetails.html";
