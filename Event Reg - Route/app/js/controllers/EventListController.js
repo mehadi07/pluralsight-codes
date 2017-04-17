de184571b242d387d93ff40234583c5f282f6212
mehadi07 <saimum22@gmail.com>
2017-04-17 18:04:45 -0400

    Unit Testing

--- Event Reg - Route/app/js/controllers/EventListController.js
+++ Event Reg - Route/app/js/controllers/EventListController.js
@@ -1,5 +1,5 @@
 'use strict';
 
-eventsApp.controller('EventListController', function EventListController($scope, $location, eventData) {
+eventsApp.controller('EventListController', function EventListController($scope, eventData) {
     $scope.events = eventData.getAllEvents();
 });
